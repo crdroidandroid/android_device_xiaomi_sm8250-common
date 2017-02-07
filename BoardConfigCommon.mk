411879c0c6d14a7ef621101cee111e1df49fde5e
Peng Xu <pxu@qca.qualcomm.com>
2017-02-07 15:16:52 -0800

    sm8250-common: Enable support for IEEE80211AC
     * This is to allow supporting WiFi 6 mobile hotspots,
       to be noted this is only supported on SDM855+ platforms.
    
    Change-Id: Iab61bd12499968e8823b3078acd1c7b8bce46136

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -243,5 +243,8 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
+CONFIG_ACS := true
+CONFIG_IEEE80211AC := true
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm8250-common/BoardConfigVendor.mk
