8fb82706557b832c2838968d7682c022a33dc4ed
TheScarastic <warabhishek@gmail.com>
2018-12-28 15:13:24 -0500

    sm8250-common: parts: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -34,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        DiracUtils.initialize(context);
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
     }
