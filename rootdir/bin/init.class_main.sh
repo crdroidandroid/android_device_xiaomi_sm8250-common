64e7899eabb6edd21c69230469ccea6740357a81
Kaushik Sadashiva Reddy <kausredd@codeaurora.org>
2020-10-02 10:06:02 -0700

    sm8250-common: rootdir: Remove legacy ril-daemon rules
    Change-Id: Id06fdf157d35779f6fd1b28c1e53794a3961a654
    CRs-Fixed: 2791395

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -40,6 +40,7 @@ case "$baseband" in
     "apq" | "sda" | "qcs" )
 #    setprop ro.vendor.radio.noril yes
     setprop ro.vendor.radio.noril true
+    stop vendor.ril-daemon
     stop vendor.qcrild
 esac
 
