74e477b5c6bdd8cc494efe096d234f90b8a89557
GtrCraft <nickvanbokhorst@hotmail.com>
2021-08-24 14:28:05 +0200

    sm8250-common: Remove MusicFX
    * We have dolby
    
    Change-Id: Id01c6420b961d2f459101c3565f9ce24d174b202

--- kona.mk
+++ kona.mk
@@ -368,6 +368,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
+    
+# RemovePackages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # RIL
 PRODUCT_PACKAGES += \
