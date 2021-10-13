47009891b163e4a0e3d3a295bd44b68b023d68c4
Jiju Kinattingal <jijuk@codeaurora.org>
2021-10-13 12:24:27 +0530

    sm8250-common: rootdir: Stop all qcrilds on APQ target
    Stop all qcrilds on APQ target
    
    Change-Id: Ieb0a6efcc61e53798650a7cf543aec6288f2947d

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -41,6 +41,8 @@ case "$baseband" in
     setprop ro.vendor.radio.noril true
     stop vendor.ril-daemon
     stop vendor.qcrild
+    stop vendor.qcrild2
+    stop vendor.qcrild3
 esac
 
 case "$baseband" in
