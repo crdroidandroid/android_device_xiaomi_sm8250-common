9d0b863692a34ceddb471dc9d12932631bcc94aa
GtrCraft <nickvanbokhorst@hotmail.com>
2021-12-28 15:31:40 +0100

    sm8250-common: Switch to crDroid doze package
    Change-Id: I5e8124704585b26abd1abc21dedb35020abda979

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.touchsampling.TouchSamplingUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DozeUtils.checkDozeService(context);
         TouchSamplingUtils.restoreSamplingValue(context);
         ThermalUtils.startService(context);
     }
