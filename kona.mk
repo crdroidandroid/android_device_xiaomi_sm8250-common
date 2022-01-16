63940056c00d3bc9a5e6305167204b053a754e2c
pkm774 <mprabhat774@gmail.com>
2022-01-16 18:11:23 +0100

    sm8250-common: Build missing sensor packages

--- kona.mk
+++ kona.mk
@@ -348,8 +348,11 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
+    android.hardware.sensors@2.0 \
+    android.hardware.sensors@2.1 \
     libsensorndkbridge
 
 # Soong namespaces
