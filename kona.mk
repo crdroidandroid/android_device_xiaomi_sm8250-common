eca708bd3f060c9ddbef59e52adb6c293e40ef74
Demon000 <demonsingur@gmail.com>
2022-01-16 17:36:25 +0100

    [SQUASH] sm8250-common: Import fingerprint HIDL
    * Uprev fingerprint HAL to 2.3
    * Implement TouchFeature
    
    Co-Authored-by: Abhishek Aggarwal <warabhishek@gmail.com>

--- kona.mk
+++ kona.mk
@@ -197,6 +197,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_kona
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
