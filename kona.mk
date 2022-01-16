d8b8d4a777051d25684c822c49070bb0be1ef781
TH779 <i@779.moe>
2022-01-16 16:47:33 +0100

    sm8250-common: Switch to prebuilt IFAAService
    * Starting from Android 12, non-root namespaces will
      no longer be allowed to use the PRODUCT_BOOT_JARS flag,
      which will cause org.ifaa.android.manager to not work
      Without creating a new repository
    
      We can also import the prebuilt IFAAService from stock ROM
      to make Alipay fingerprint payment work
    
    Change-Id: Ib1640e5cae6dcbb09e53af6037ddf966ed988d23
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- kona.mk
+++ kona.mk
@@ -230,9 +230,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# IFAA manager
-    include device/xiaomi/extras/ifaa.mk
-
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
