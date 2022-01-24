c3b3db0a103c4977399acccb64ec171b2b48de6d
jjpprrrr <jjpprrrr@pixelexperience.org>
2022-01-24 14:59:03 +0100

    sm8250-common: Use IFAA Manager from a seperated repo * platform-bootclasspath cannot depend on something from a non-root   soong namespace

--- kona.mk
+++ kona.mk
@@ -297,6 +297,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+    include device/xiaomi/extras/ifaa.mk
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
