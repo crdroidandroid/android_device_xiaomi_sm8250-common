71a2e457f18fa8336860dae52d96922f07f6755b
¨raystef66¨ <s.vanbarel@gmail.com>
2022-03-19 12:23:40 +0100

    sm8250-common: set vendor sec patch level to 2022.01.01 *updated to miui_LMIGlobal_V12.5.6.0.RJKMIXM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -206,7 +206,7 @@ SOONG_CONFIG_XIAOMI_KONA_ROOTDIR_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
+VENDOR_SECURITY_PATCH := 2022-01-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
