d71fe6d8c918d7402d3d9f6d370b38ef64c32971
Pranav Vashi <neobuddy89@gmail.com>
2022-04-09 11:49:15 +0530

    sm8250-common: parts: Set default refresh rate to 120Hz

--- parts/src/org/lineageos/settings/refreshrate/RefreshUtils.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshUtils.java
@@ -42,7 +42,7 @@ public final class RefreshUtils {
     protected static final int STATE_EXTREME = 3;
     protected static final int STATE_ULTRA = 4;
 
-    private static final float REFRESH_STATE_DEFAULT = 60f;
+    private static final float REFRESH_STATE_DEFAULT = 120f;
     private static final float REFRESH_STATE_STANDARD = 60f;
     private static final float REFRESH_STATE_HIGH = 90f;
     private static final float REFRESH_STATE_EXTREME = 120f;
@@ -70,8 +70,8 @@ public final class RefreshUtils {
     }
 
    protected void getOldRate(){
-        defaultMaxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, 60);
-        defaultMinRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, 60);
+        defaultMaxRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_PEAK_REFRESH_RATE, REFRESH_STATE_DEFAULT);
+        defaultMinRate = Settings.System.getFloat(mContext.getContentResolver(), KEY_MIN_REFRESH_RATE, REFRESH_STATE_DEFAULT);
     }
 
 
