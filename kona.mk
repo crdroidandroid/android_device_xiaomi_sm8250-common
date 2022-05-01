3cf2892f0e82e962b4350928e942d9c75110d7d2
¨raystef66¨ <s.vanbarel@gmail.com>
2022-05-01 13:47:32 +0200

    sm8250-common: build com.dsi.ant@1.0.vendor

--- kona.mk
+++ kona.mk
@@ -124,7 +124,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    com.dsi.ant@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
