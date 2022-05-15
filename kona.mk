f09ce92fd69be8886436fe2d2ed42f5d8b97516e
johnmart19 <johnivan19999@gmail.com>
2022-05-15 08:25:29 +0300

    sm8250-common: Implement support of vendor.qti.hardware.btconfigstore@2.0
    - Import blobs from Alioth Miui 13.0.8.0 Europe
    - Import vendor.qti.hardware.fm impl, declare to compile hal
    - Import com.dsi.ant impl, declare to compile hal
    - Declare to use vendor.qti.hardware.bluetooth_audio@2.0-1

--- kona.mk
+++ kona.mk
@@ -153,7 +153,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.fm@1.0.vendor
 
 # Bluetooth Library Deps
 PRODUCT_PACKAGES += \
@@ -169,6 +170,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libldacBT_enc \
     libldacBT_abr
+
+# Bluetooth Ant+ Hal Deps
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant.antradio_library \
+    com.dsi.ant@1.0.vendor
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Camera
 PRODUCT_PACKAGES += \
