034b180b0a7f66c114be9a7a76ca1333e9d32aed
¨raystef66¨ <s.vanbarel@gmail.com>
2022-05-20 13:20:49 +0200

    sm8250-common: Build vendor.qti.hardware.servicetracker@1.2 / import from V13.0.3.0.SKHMIXM

--- kona.mk
+++ kona.mk
@@ -544,6 +544,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1 \
     libsensorndkbridge
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-service \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
