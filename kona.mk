adb16bef108f7dbe4e55b7649ad2a3ffbd71e42f
johnmart19 <johnivan19999@gmail.com>
2022-08-09 20:51:20 +0300

    sm8250-common: Build android.frameworks.sensorservice@1.0

--- kona.mk
+++ kona.mk
@@ -209,6 +209,7 @@ TARGET_ENABLE_BLUR := true
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
