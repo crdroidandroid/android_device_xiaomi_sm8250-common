ccc644ab3e5d3adebb0b9fbe6f1cbea16b79a83a
mikairyuu <miva200267@gmail.com>
2022-09-05 13:08:37 +0300

    sm8250-common: Build android.hardware.bluetooth.a2dp

--- kona.mk
+++ kona.mk
@@ -149,6 +149,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
