fdd576ecd0d87d0c7534a0844d6cfbc9b710f55b
johnmart19 <johnivan19999@gmail.com>
2022-09-15 03:59:04 +0300

    sm8250-common: extract-files: Cleanup Dolby related changes
    - This patch is doing absolutely nothing, AOSP does not load media_profiles_kona.xml , it loads media_profiles_vendor.xml
    - We will need to patch media_profiles_vendor.xml to actually load Dolby Media codecs, otherwise that file is skipped and codecs are not loaded

--- extract-files.sh
+++ extract-files.sh
@@ -58,14 +58,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-        vendor/etc/media_codecs_kona.xml)
-            sed -i "/media_codecs_dolby_audio.xml/d" "${2}"
-            ;;
-    esac
-}
-
 if [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
