4f268ea69c876a08c10749513aead6a259b533d1
johnmart19 <johnivan19999@gmail.com>
2022-09-21 00:37:14 +0300

    sm8250-common: BT: Import more LDAC deps & Import back legacy BT Audio Target
    - Required for our current Libs

--- kona.mk
+++ kona.mk
@@ -150,6 +150,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
@@ -164,6 +165,10 @@ PRODUCT_PACKAGES += \
     liblhdcBT_enc \
     liblhdcdec \
     liblhdcBT_dec
+
+PRODUCT_PACKAGES += \
+    libldacBT_enc \
+    libldacBT_abr
 
 # Camera
 PRODUCT_PACKAGES += \
