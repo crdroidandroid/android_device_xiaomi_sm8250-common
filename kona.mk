4eb5f4322d49f3e59adb26912638369b9988f99c
Adam Lawson <adamjlawson22@gmail.com>
2022-09-22 22:44:33 +0530

    sm8250-common: Build Codec2 Packages on vendor
    libcodec2_vndk.vendor
    android.hardware.media.c2@1.0.vendor
    libcodec2_hidl@1.0.vendor
    Required since hals not included in vndk

--- kona.mk
+++ kona.mk
@@ -187,6 +187,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Codec2
+PRODUCT_PACKAGES += \
+    libcodec2_vndk.vendor \
+    android.hardware.media.c2@1.0.vendor \
+    libcodec2_hidl@1.0.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
