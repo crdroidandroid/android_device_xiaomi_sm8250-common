4c7c678e15740f13195133e7fefa66c3416354a2
johnmart19 <johnivan19999@gmail.com>
2022-10-08 02:13:09 +0300

    sm8250-common: Build legacy android.frameworks.sensorservice@1.0
    [   19.866353] init: Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 637 (/system/bin/hwservicemanager)

--- kona.mk
+++ kona.mk
@@ -511,7 +511,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
