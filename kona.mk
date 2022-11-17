b57675ca6b720a53f9e70302a4811fa793c8b453
Cyber Knight <cyberknight755@gmail.com>
2022-11-17 14:42:43 +0800

    sm8250-common: Add an RRO to show default KProfiles Mode
    - This essentially overrides what's set in packages/apps/KProfiles with what I have set.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: akash07k <akash.galaxy07@gmail.com>
    Signed-off-by: Akash Kakkar <6381747+akash07k@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -354,6 +354,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-arrow
 
+# KProfiles overlays
+PRODUCT_PACKAGES += \
+    KProfilesOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
