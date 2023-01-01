c64e8d4ce2fd6868d98c3961cd4d6d1dde90fd3e
Akash Kakkar <akash.galaxy07@gmail.com>
2023-01-01 19:11:32 +0530

    sm8250-common: Removed libdisplayconfig.qti.vendor and libhidl modules
    Signed-off-by: akash07k <akash.galaxy07@gmail.com>

--- kona.mk
+++ kona.mk
@@ -191,7 +191,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
