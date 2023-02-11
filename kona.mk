9ec0103cea786ce83c2b487b7c154be3549c9005
Mathieu Chartier <mathieuc@google.com>
2023-02-11 10:17:52 +0000

    sm8250-common: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -396,6 +396,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
