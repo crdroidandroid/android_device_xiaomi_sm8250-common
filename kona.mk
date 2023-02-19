f1f613073ccf1f85ec59ebfdd2f4c0cf425b7747
akash07k <akash.galaxy07@gmail.com>
2023-02-19 21:16:29 +0530

    sm8250-common: Change to lineage
    Signed-off-by: Akash Kakkar <6381747+akash07k@users.noreply.github.com>

--- kona.mk
+++ kona.mk
@@ -352,7 +352,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-lineage
 
 # KProfiles overlays
 PRODUCT_PACKAGES += \
