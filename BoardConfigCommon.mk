3f2bd2434bd61792736cf42e613e61222debf3b5
johnmart19 <johnivan19999@gmail.com>
2023-02-26 12:48:21 +0200

    sm8250-common: Import Xiaomi TouchFeature service

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -208,6 +208,7 @@ endif
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
