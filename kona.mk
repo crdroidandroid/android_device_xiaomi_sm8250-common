448177a52c83848d30955b43565b5907af2c2c49
akash07k <6381747+akash07k@users.noreply.github.com>
2023-03-13 01:32:24 +0530

    sm8250-common: VAB: Inherit from Android 13, Enable XOR compression and userspace merge

--- kona.mk
+++ kona.mk
@@ -67,6 +67,13 @@ PRODUCT_COPY_FILES += \
 ifeq ($(TARGET_IS_VAB),true)
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/android_t_baseline.mk)
+
+# Enable compression with xor
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
+
+# Enable userspace merge
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.userspace.snapshots.enabled=true
 
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti \
