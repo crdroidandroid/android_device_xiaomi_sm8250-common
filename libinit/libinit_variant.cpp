2844df51ba9644d99246eab762d037a6a2fb90df
Eidoron1 <eidoronp67@gmail.com>
2023-03-26 20:56:14 +0545

    sm8250-common: Add support for miui camera

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -32,6 +32,7 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("brand", variant.brand, true);
     set_ro_build_prop("device", variant.device, true);
     set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("mod_device", variant.mod_device, true);
     set_ro_build_prop("model", variant.model, true);
 
     if (access("/system/bin/recovery", F_OK) != 0) {
