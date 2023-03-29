9656f821c97dda41b0083ff2c839cbc0a7dedc48
¨raystef66¨ <s.vanbarel@gmail.com>
2023-03-29 18:38:16 +0200

    sm8250-common: drop com.dsi.ant.antradio_library

--- kona.mk
+++ kona.mk
@@ -216,11 +216,7 @@ PRODUCT_PACKAGES += \
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
-    com.dsi.ant.antradio_library \
     com.dsi.ant@1.0.vendor
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # Camera
 PRODUCT_PACKAGES += \
