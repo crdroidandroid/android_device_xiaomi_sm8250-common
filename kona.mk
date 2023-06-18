34fa1823a5f97b8b8ddfd81e006d551280858638
Luofan Chen <clfbbn@gmail.com>
2023-06-18 14:07:09 +0200

    sm8250-common: Build lineage health HAL

--- kona.mk
+++ kona.mk
@@ -361,6 +361,10 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
+	
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
