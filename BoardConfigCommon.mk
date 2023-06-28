462243e6a6ec97dadefa8166c75505ea96ce460e
Chris Fries <cfries@google.com>
2023-06-28 16:50:04 +0200

    sm8250-common: Set VENDOR_SECURITY_PATCH to platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -209,7 +209,7 @@ SOONG_CONFIG_XIAOMI_KONA_ROOTDIR_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-01-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
