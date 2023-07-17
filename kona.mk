7d2d7473c4479f286c7e13c67ef7e2768f85a5e3
1582130940 <19632674+1582130940@users.noreply.github.com>
2023-07-17 17:18:57 +0800

    sm8250-common: device: Sets Android Go recommended default product options
    * Speed profile services and wifi-service to reduce RAM and storage
    Mathieu Chartier <mathieuc@google.com>
    
    * Inherit several Android Go configurations
    Albert I <kras@raphielgang.org>
    
    Co-authored-by: Mathieu Chartier <mathieuc@google.com>
    Co-authored-by: Albert I <kras@raphielgang.org>

--- kona.mk
+++ kona.mk
@@ -163,6 +163,29 @@ endif
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dex
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
+# Use a profile based boot image for this device. Note that this is currently a
+# generic profile and not Android Go optimized.
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
+# Do not generate libartd.
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Disable Scudo outside of eng builds to save RAM.
+PRODUCT_DISABLE_SCUDO := true
+
+# Dedupe VNDK libraries with identical core variants.
+TARGET_VNDK_USE_CORE_VARIANT := true
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
