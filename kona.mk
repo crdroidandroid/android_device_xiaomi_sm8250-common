e06fd6fec657fe099c75b2758dcbb92070c85520
ghostrider-reborn <adithya.r02@outlook.com>
2023-07-17 17:29:27 +0800

    sm8250-common: device: Optimize ART
    * Disable ART debug and optimize dexpreopt
    * Disable dexpreopt minidebuginfo
    ghostrider-reborn <adithya.r02@outlook.com>

--- kona.mk
+++ kona.mk
@@ -99,6 +99,14 @@ PRODUCT_PACKAGES += \
     OpenDeltaOverlayKona
 endif
 
+# ART
+ART_BUILD_TARGET_NDEBUG := true
+ART_BUILD_TARGET_DEBUG := false
+ART_BUILD_HOST_NDEBUG := true
+ART_BUILD_HOST_DEBUG := false
+USE_DEX2OAT_DEBUG := false
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
