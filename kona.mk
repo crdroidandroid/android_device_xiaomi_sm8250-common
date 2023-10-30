77d34c369f9b4f08280cf5a12c79d443f6232128
¨raystef66¨ <s.vanbarel@gmail.com>
2023-10-30 17:49:08 +0100

    Revert "sm8250-common: Implement SunlightEnhancement LiveDisplay HAL"
    This reverts commit 72f45031696c9acfa4282a8403babc1944007c93.

--- kona.mk
+++ kona.mk
@@ -370,8 +370,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.alioth
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
