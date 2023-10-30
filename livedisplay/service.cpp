9f56eede96b9357dfdd6df65c885c6366c580882
¨raystef66¨ <s.vanbarel@gmail.com>
2023-10-30 17:48:36 +0100

    Revert "sm8250-common: Livedisplay: Update to version 2.1"
    This reverts commit 7bdd9118e65e0d38919a6a749d618612c1be55cc.

--- livedisplay/service.cpp
+++ livedisplay/service.cpp
@@ -14,34 +14,24 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "vendor.lineage.livedisplay@2.1-service.alioth"
+#define LOG_TAG "vendor.lineage.livedisplay@2.0-service.alioth"
 
 #include <android-base/logging.h>
 #include <binder/ProcessState.h>
 #include <hidl/HidlTransportSupport.h>
 
 #include "SunlightEnhancement.h"
-#include "livedisplay/sdm/SDMController.h"
 
-using android::OK;
-using android::sp;
-using android::status_t;
-
-using ::vendor::lineage::livedisplay::V2_0::sdm::SDMController;
-using ::vendor::lineage::livedisplay::V2_1::ISunlightEnhancement;
-using ::vendor::lineage::livedisplay::V2_1::implementation::SunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
+using ::vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
 
 int main() {
-    status_t status = OK;
-    std::shared_ptr<SDMController> controller = std::make_shared<SDMController>();
-    sp<SunlightEnhancement> se = new SunlightEnhancement();
+    android::sp<ISunlightEnhancement> sunlightEnhancement = new SunlightEnhancement();
+
     android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
 
-    // SunlightEnhancement service
-    status = se->registerAsService();
-    if (status != OK) {
-        LOG(ERROR) << "Could not register service for LiveDisplay HAL SunlightEnhancement Iface ("
-                   << status << ")";
+    if (sunlightEnhancement->registerAsService() != android::OK) {
+        LOG(ERROR) << "Cannot register sunlight enhancement HAL service.";
         return 1;
     }
 
