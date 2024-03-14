c72e8584b0b1c714ab8a8012a24b0966d1354b1d
Jake Weinstein <jake@aospa.co>
2024-03-14 06:11:29 +0530

    sm8250-common: Do not balance msm_drm and kgsl_3d0 IRQs
    These are critical for display performance and should not be balanced
    in order to improve latency and responsiveness.
    
    Change-Id: I29a6fc097b7fac41e7fce99db6fd453f179ac973
    Signed-off-by: SenseiiX <senseixdev@gmail.com>

--- kona.mk
+++ kona.mk
@@ -362,7 +362,8 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # Public libraries
 PRODUCT_COPY_FILES += \
