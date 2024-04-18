9f82026e293c3e698aa32283b8cda4b10d1e3fe1
johnmart19 <johnivan19999@gmail.com>
2024-04-18 07:37:53 +0300

    sm8250-common: Drop LMI and Popup Camera support
    - We are not going to build for devices that using Popup camera
    - Remove reurdant parts and declarations to keep code clean
    - Switch proguard to keep all elements
    - Remove lmi support as it utilizes popup camera

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
 
-import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
 
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
     }
