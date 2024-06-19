742dbd8590893c445a744048f8981e2b68460e4e
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-19 11:26:40 +0800

    sm8250-common: Build AOSP Radio HIDL->AIDL Wrapper
    Depends-On: I790728a9be289042e5a07589934a2dd50463a507
    Change-Id: Iaeb8d37f4a7f02e6e812bc4da1b413c9a6493f8f

--- kona.mk
+++ kona.mk
@@ -300,6 +300,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# Ril
+PRODUCT_PACKAGES += \
+    android.hardware.radio-service.compat
+
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
