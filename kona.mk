b07fb98799e3de5d9ccabb485021986a845ef2d8
johnmart19 <johnivan19999@gmail.com>
2024-10-31 04:00:38 +0200

    sm8250-common: Import Dolby MediaCodecs dependenices & Fix Loading Mediacodecs
    Co-authored-by: Flicker372 <flicker372@outlook.com>

--- kona.mk
+++ kona.mk
@@ -166,6 +166,13 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.vendor.audio.dolby.dax.support=true \
     ro.vendor.audio.dolby.surround.enable=true
 
+# Dolby MediaCodecs Dependenices
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.0.vendor \
+    libcodec2_soft_common.vendor \
+    libstagefright_softomx.vendor \
+    libstagefright_softomx_plugin.vendor
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
@@ -260,11 +267,6 @@ PRODUCT_PACKAGES += \
     media_codecs_kona.xml \
     media_codecs_performance_kona.xml \
     video_system_specs.json
-
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
-
 
 # NFC
 PRODUCT_PACKAGES += \
