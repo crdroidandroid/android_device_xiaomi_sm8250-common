ddfbec8a242280bfcc26a184cf37aa7195508bd0
johnmart19 <johnivan19999@gmail.com>
2024-12-22 22:45:14 +0200

    sm8250-common: libwfdnative: add libbinder shim
    During the compilation:
    error: Unresolved symbol: _ZN7android6binder10atrace_endEm@LIBBINDER
    error: Unresolved symbol: _ZN7android6binder12atrace_beginEmPKc@LIBBINDER

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,7 @@ function blob_fixup() {
             ;;
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
+            grep -q "libbinder_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
             grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
         system_ext/lib64/libwfdmmsrc_system.so)
