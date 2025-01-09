730e6e9d86e61e994fa593cd1180994a483ad65a
LuK1337 <priv.luk@gmail.com>
2025-01-09 12:57:53 +0100

    sm8250-common: Build gralloc.qcom
    Camera HAL wants it and crashes if it loads default gralloc
    implementation instead.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/8059
    Change-Id: I47965713c822cfdfc369975f8975f21f70133608

--- kona.mk
+++ kona.mk
@@ -178,6 +178,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service
 
 PRODUCT_PACKAGES += \
+    gralloc.qcom
+
+PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
 $(call soong_config_set,qtidisplay,gralloc_handle_has_reserved_size,true)
