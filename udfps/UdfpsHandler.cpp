060fe8622be5e123a2d0ebb7b7f7d04e47946826
Fuutao <fuutaoao@gmail.com>
2025-01-20 18:17:07 +0800

    Revert "sm8250-common: udfps: Wire up Xiaomi's touchfeature"
    This reverts commit 2d9367fc82b6937560187b0aa6fe20d730891082.

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -9,7 +9,6 @@
 #include "UdfpsHandler.h"
 
 #include <android-base/logging.h>
-#include <android-base/unique_fd.h>
 #include <fcntl.h>
 #include <poll.h>
 #include <thread>
@@ -18,14 +17,6 @@
 #define COMMAND_NIT 10
 #define PARAM_NIT_FOD 1
 #define PARAM_NIT_NONE 0
-
-#define FOD_STATUS_ON 1
-#define FOD_STATUS_OFF -1
-
-#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
-#define TOUCH_FOD_ENABLE 10
-#define TOUCH_MAGIC 0x5400
-#define TOUCH_IOC_SETMODE TOUCH_MAGIC + 0
 
 static const char* kFodUiPaths[] = {
         "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_ui",
@@ -59,8 +50,6 @@ class XiaomiKonaUdfpsHandler : public UdfpsHandler {
   public:
     void init(fingerprint_device_t *device) {
         mDevice = device;
-
-        touch_fd_ = android::base::unique_fd(open(TOUCH_DEV_PATH, O_RDWR));
 
         std::thread([this]() {
             int fd;
@@ -107,13 +96,11 @@ class XiaomiKonaUdfpsHandler : public UdfpsHandler {
     }
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
-        int arg[2] = {TOUCH_FOD_ENABLE, FOD_STATUS_ON};
-        ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
+        // nothing
     }
 
     void onFingerUp() {
-        int arg[2] = {TOUCH_FOD_ENABLE, FOD_STATUS_OFF};
-        ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
+        // nothing
     }
 
     void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
@@ -137,7 +124,6 @@ class XiaomiKonaUdfpsHandler : public UdfpsHandler {
     }
   private:
     fingerprint_device_t *mDevice;
-    android::base::unique_fd touch_fd_;
 };
 
 static UdfpsHandler* create() {
