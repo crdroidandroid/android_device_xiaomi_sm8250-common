a62ed8813a330e0c9299c237024e4d535c6c5e40
EmanuelCN <emanuelghub@gmail.com>
2025-01-29 14:40:01 +0530

    sm8250-common: Override cgroups.json & task_profiles.json

--- kona.mk
+++ kona.mk
@@ -133,6 +133,11 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
+# Cgroup and task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
