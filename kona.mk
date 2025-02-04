f02e04410fee9d2fe4d8e947cecd1a71a1d839eb
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-04 22:03:11 +0100

    sm8250-common: Move ramdisk fstab target to Soong
    Change-Id: I1e3c56a6ab44a3e64021e23d5e6e3d8e972071b4

--- kona.mk
+++ kona.mk
@@ -312,7 +312,8 @@ PRODUCT_PACKAGES += \
 # Rootdir
 PRODUCT_PACKAGES += \
     fstab.qcom \
-    fstab.qcom_ramdisk
+    fstab.qcom.ramdisk \
+    fstab.qcom.vendor_ramdisk
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,17 +9,3 @@ LOCAL_MODULE_CLASS     := ETC
 LOCAL_SRC_FILES        := etc/fstab.zram
 LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom_ramdisk
-LOCAL_MODULE_STEM  := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
-LOCAL_SRC_FILES    := etc/fstab_AB.qcom
-LOCAL_MODULE_PATH  := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
-else
-LOCAL_SRC_FILES    := etc/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
-endif
-include $(BUILD_PREBUILT)
