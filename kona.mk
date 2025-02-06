b257cefc32099fe1a248d8ab1aa18ee43af15067
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    sm8250-common: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- kona.mk
+++ kona.mk
@@ -74,6 +74,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
