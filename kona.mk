57be5412cb6293fe719400baa07372723aca88d4
¨raystef66¨ <s.vanbarel@gmail.com>
2025-02-08 08:45:23 +0100

    sm8250-common: remove offending OMX entries

--- kona.mk
+++ kona.mk
@@ -421,12 +421,7 @@ PRODUCT_COPY_FILES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
