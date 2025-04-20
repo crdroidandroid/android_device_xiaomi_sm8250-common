5a084daaee1975a2a6419b52a456b16350c28b09
CuriousNom <aryanbiswas7585@gmail.com>
2025-04-20 18:32:33 +0530

    sm8250-common: Configure custom thermal

--- kona.mk
+++ kona.mk
@@ -343,7 +343,8 @@ PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
-    init.qti.dcvs.sh
+    init.qti.dcvs.sh \
+    init.mi.thermal.sh
 
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \
@@ -351,7 +352,8 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.mi.thermal.rc
 
 # Sensors
 PRODUCT_PACKAGES += \
