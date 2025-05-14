01d23bfac740913e672dd3546f1ab214edcde144
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-05-14 13:51:29 +0000

    sm8550-common: Build missing android.hidl.base@1.0 from source This is needed for WfdService to not crash.
    Fixes the error: dlopen failed: library android.hidl.base@1.0.so not found: needed by /system_ext/lib64/libwfdnative.so

--- kona.mk
+++ kona.mk
@@ -161,6 +161,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
 
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     vendor.qti.hardware.display.allocator-service
