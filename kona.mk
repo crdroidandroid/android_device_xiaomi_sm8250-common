3fb997451d9d4e9d9236f07d6b137f8ea4973908
IPSBHANGU <inderpreetbhangu2@gmail.com>
2025-05-14 13:38:50 +0000

    sm8250-common: Build libusbhost to vendor

--- kona.mk
+++ kona.mk
@@ -444,6 +444,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
+
+PRODUCT_PACKAGES += \
+    libusbhost.vendor
+
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
