798eca10891c091e761a4f59188a75afb0603722
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-06-07 09:57:56 +0200

    sm8250-common: switch to common AIDL IR service

--- kona.mk
+++ kona.mk
@@ -238,8 +238,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.lineage
 
 # Component overrides
 PRODUCT_COPY_FILES += \
