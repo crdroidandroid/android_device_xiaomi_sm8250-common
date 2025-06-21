1e46749963e98f4036842e3cb244c490304f9db3
LuK1337 <priv.luk@gmail.com>
2025-06-21 13:44:53 +0200

    sm8250-common: Enable QTI recovery minui backend
    Change-Id: Ia6cd1edeb5f4b1512a1345b1fec51c11129ed068

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,6 +149,7 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor_phone.prop
 endif
 
 # Recovery
+$(call soong_config_set_bool,recovery,target_recovery_uses_qti_drm,true)
 ifeq ($(TARGET_IS_VAB),true)
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab_AB.qcom
 else
