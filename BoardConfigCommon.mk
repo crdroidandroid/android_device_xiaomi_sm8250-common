74e1a432598997596fe90676f098015386f85da7
glitch-wraith <sheoranpranshu@gmail.com>
2025-08-01 19:57:21 +0530

    Revert "sm8250-common: Include Sony Dolby Atmos"
    This reverts commit 9f6a013c513439ff74557528e42fcfe0086a5f43.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -213,8 +213,7 @@ BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 endif
 
 # VINTF
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/framework_compatibility_dolby.xml \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
--- kona.mk
+++ kona.mk
@@ -197,9 +197,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
-# Dolby
-$(call inherit-product, hardware/dolby/dolby.mk)
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
