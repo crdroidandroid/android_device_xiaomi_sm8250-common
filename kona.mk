00cff28e58c94e9d51ca03f9206a75db1a87c928
johnmart19 <johnivan19999@gmail.com>
2025-08-23 08:27:14 +0300

    sm8250-common: Move to the common WFD stack

--- kona.mk
+++ kona.mk
@@ -415,6 +415,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # Wi-Fi Display
+$(call inherit-product, device/qcom/wfd/device.mk)
+
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
 
