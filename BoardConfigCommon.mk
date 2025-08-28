c8e00d94ce024d39d7aeec711d9cb9f2ddb5fb64
¨raystef66¨ <s.vanbarel@gmail.com>
2025-08-28 13:49:00 +0200

    sm8250-common: Set camera package name via soong config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -70,7 +70,7 @@ TARGET_NO_BOOTLOADER := true
 MALLOC_SVELTE_FOR_LIBC32 := true
 
 # Camera - Miui
-TARGET_CAMERA_PACKAGE_NAME := com.android.camera
+$(call soong_config_set,camera,package_name,com.android.camera)
 
 # Camera - HIDL overrideFormat
 $(call soong_config_set,camera,override_format_from_reserved,true)
