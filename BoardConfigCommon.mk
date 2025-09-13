635d405d7fad2c425b9e67d288a6f99805cdab1c
Chris Fries <cfries@google.com>
2025-09-13 09:08:16 +0200

    sm8250-common: Set VENDOR_SECURITY_PATCH to platform Set the VENDOR_SECURITY_PATCH level to match the platform security patch level.  These are functionally the same.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -197,7 +197,7 @@ SOONG_CONFIG_XIAOMI_KONA_PARTITION_SCHEME := a
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-04-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
