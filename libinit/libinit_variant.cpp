162c07a1be97a0b2c1bdd08a3174f9aaf71fefd7
Kenaidi01 <andrianp920@gmail.com>
2025-12-18 17:24:07 +0700

    sm8250-common: libinit: Drop fingerprint override [1/2]
    Signed-off-by: Kenaidi01 <andrianp920@gmail.com>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -49,11 +49,6 @@ void set_variant_props(const variant_info& variant) {
 
     if (access("/system/bin/recovery", F_OK) != 0) {
         property_override("bluetooth.device.default_name", marketname, true);
-        set_ro_build_prop("fingerprint", variant.build_fingerprint);
-        property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
-
-        property_override("ro.build.description",
-                          fingerprint_to_description(variant.build_fingerprint));
     }
 
     if (variant.nfc) {
